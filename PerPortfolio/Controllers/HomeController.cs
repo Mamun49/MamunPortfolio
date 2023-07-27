7153cfe7543c51f7e8e3bbdcfe4d337bc124cdbc
mamun49 <ovijust@gmail.com>
2023-07-27 23:43:53 +0600

    map size

---
+++ PerPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Rotativa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PerPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+           
+
+            return View();
+        }
+        
+        public ActionResult Download()
+        {
+            string fileName = "Resume of Md Hasan Al Mamun.pdf";
+            string filePath = Server.MapPath("~/Content/" + fileName);
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+
+            return File(fileBytes, "application/pdf", fileName);
+        }
+    }
+}
